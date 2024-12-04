9611ec891a7d647ac8a130ef84fadf4bff6fcd8c
unknown <mhriad786@gmail.com>
2024-12-04 13:49:38 +0600

    firebase setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import router from './Router/router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './provider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>,
 )
